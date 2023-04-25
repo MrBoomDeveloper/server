be5d30aec27338f4b75cd6c90514f897000bb133
MrBoomDev <github@mrboomdev.ru>
2023-04-26 01:16:29 +0500

    a

--- index.ts
+++ index.ts
@@ -1,3 +1,27 @@
-import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+import { listenAndServe, ServerRequest } from "https://deno.land/std@v0.51.0/http/server.ts";
+import { createRouter, AugmentedRequest, createRouteMap, textResponse, jsonResponse } from "https://deno.land/x/reno@v1.0.0-alpha.1/reno/mod.ts";
 
-serve((req: Request) => Response.json({ message: "Hello World, Amoguses!", amir: "Здаров Амир!" }));
+export const routes = createRouteMap([
+	["/", () => textResponse("Hello, World!")],
+	["/news/action", () => jsonResponse([
+		{ title: "Test 1", description: "hi" },
+		{ title: "Test 2", description: "hey!" }
+	]),
+	["/gamemodes", () => jsonResponse([
+		{ title: "Test 1", description: "hi" },
+		{ title: "Test 2", description: "hey!" }
+	])
+]);
+
+const router = createRouter(routes);
+(async () => {
+	console.log("Listening for requests...");
+	await listenAndServe(":8001", async (req: ServerRequest) => {
+		try {
+			const res = await router(req);
+			return req.respond(res);
+		} catch (e) {
+			return req.respond(e);
+		}
+	});
+})();
