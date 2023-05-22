73dd60c676bdc7434fa29da6c9cb4f23da4128b8
MrBoomDev <github@mrboomdev.ru>
2023-05-22 18:20:44 +0500

    Base things, Config file

--- index.ts
+++ index.ts
@@ -1,27 +1,40 @@
-import { listenAndServe, ServerRequest } from "https://deno.land/std@v0.51.0/http/server.ts";
-import { createRouter, AugmentedRequest, createRouteMap, textResponse, jsonResponse } from "https://deno.land/x/reno@v1.0.0-alpha.1/reno/mod.ts";
+import { serve } from "std/http/server.ts";
 
-export const routes = createRouteMap([
-	["/", () => textResponse("Hello, World!")],
-	["/news/action", () => jsonResponse([
-		{ title: "Test 1", description: "hi" },
-		{ title: "Test 2", description: "hey!" }
-	]),
-	["/gamemodes", () => jsonResponse([
-		{ title: "Test 1", description: "hi" },
-		{ title: "Test 2", description: "hey!" }
-	])
-]);
-
-const router = createRouter(routes);
-(async () => {
-	console.log("Listening for requests...");
-	await listenAndServe(":8001", async (req: ServerRequest) => {
-		try {
-			const res = await router(req);
-			return req.respond(res);
-		} catch (e) {
-			return req.respond(e);
+function handler(request: Request): Response {
+	console.log(JSON.stringify(request));
+	return new Response(JSON.stringify(data), {
+		status: 200,
+    	headers: {
+			"content-type": "application/json"
 		}
 	});
-})();
+}
+
+const data: News = {
+	blog: [
+		{
+			title: "title",
+			message: "idk",
+			pinned: true,
+			date: 484839394848
+		}, {
+			title: "hmm",
+			pinned: false,
+			message: "idkhhhh",
+			date: 36346
+		}
+	]
+}
+
+interface News {
+	blog: Post[]
+}
+
+interface Post {
+	title: string,
+	message: string,
+	date: number,
+	pinned: boolean
+}
+
+serve(handler);
